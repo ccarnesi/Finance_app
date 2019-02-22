16bb37a3f7e85607ae2323b9b9339db4b58988bd
Chris Carnesi <chriscarnesi@nbp-216-194.nbp.ruw.rutgers.edu>
2019-02-22 13:47:58 -0500

    First Commit

---
+++ Finance_app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Finance_app
+//
+//  Created by Chris Carnesi on 2/22/19.
+//  Copyright © 2019 Carnesi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
